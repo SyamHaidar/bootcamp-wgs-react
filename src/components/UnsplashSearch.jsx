12ccd59cc134c6974cd4012ed10e116a13f1cf55
SyamHaidar <syamhaidar@gmail.com>
2022-09-29 23:56:19 +0700

    Update image list view with grid

--- src/components/UnsplashSearch.jsx
+++ src/components/UnsplashSearch.jsx
@@ -1,10 +1,41 @@
-import { useState } from 'react'
+import { createRef, useState } from 'react'
 // style
-import { Stack, TextField } from '@mui/material'
+import { Box, Stack, TextField } from '@mui/material'
 // service API
 import unsplash from '../services/unsplash'
+import { useEffect } from 'react'
 
 // ---------------------------------------------------------------------
+
+const ImgCard = ({ image }) => {
+  const [span, setSpan] = useState(0)
+
+  const imgRef = createRef()
+
+  useEffect(() => {
+    const setSpans = () => {
+      const height = imgRef.current.clientHeight
+      const spans = Math.ceil(height / 10)
+      setSpan(spans)
+    }
+
+    if (imgRef && imgRef.current) {
+      imgRef.current.addEventListener('load', setSpans)
+    }
+  }, [imgRef, span])
+
+  return (
+    <img
+      ref={imgRef}
+      src={image.urls.regular}
+      alt={image.alt_description}
+      style={{
+        width: '100%',
+        gridRowEnd: `span ${span}`,
+      }}
+    />
+  )
+}
 
 const UnsplashSearch = () => {
   // search value
@@ -12,45 +43,41 @@ const UnsplashSearch = () => {
   // image data
   const [image, setImage] = useState([])
 
-  const getSearch = async (str) => {
+  const getSearch = async (e) => {
+    e.preventDefault()
+
     // get image data from unsplash
-    const { data } = await unsplash.get('/search/photos', { params: { query: str } })
+    const { data } = await unsplash.get('/search/photos', { params: { query: search } })
     // fill image data from unsplash.get
     setImage(data.results)
-  }
-
-  const handleSearch = (e) => {
-    // when seacrh value change get search image
-    setSearch(e.target.value)
-    getSearch(e.target.value)
   }
 
   return (
     <Stack direction="column" spacing={4}>
       {/* form input */}
-      <Stack direction="column" spacing={2} sx={{ marginTop: '12px' }}>
-        <TextField label="Search" required value={search} onChange={handleSearch} />
-      </Stack>
+      <Box sx={{ marginTop: '12px' }}>
+        <form onSubmit={getSearch}>
+          <TextField
+            label="Search"
+            required
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            sx={{ width: '100%' }}
+          />
+        </form>
+      </Box>
 
       {/* data list */}
-      <Stack direction="row" sx={{ flexWrap: 'wrap' }}>
-        {image &&
-          image.map((item, index) => (
-            <img
-              key={index}
-              src={item.urls.regular}
-              alt={item.alt_description}
-              style={{
-                height: '200px',
-                width: 'auto',
-                objectFit: 'cover',
-                flexShrink: 0,
-                marginRight: '8px',
-                marginBottom: '8px',
-              }}
-            />
-          ))}
-      </Stack>
+      <Box
+        sx={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(auto-fill, minmax(250px, 1fr))',
+          gridGap: '0 10px',
+          gridAutoRows: '10px',
+        }}
+      >
+        {image && image.map((item, index) => <ImgCard key={index} image={item} />)}
+      </Box>
     </Stack>
   )
 }
