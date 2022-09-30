9f0823933300132932c3059f19c726a8e35201f6
SyamHaidar <syamhaidar@gmail.com>
2022-09-30 13:56:48 +0700

    Add youtube route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
 // layout
 import Navbar from './layouts/Navbar'
@@ -7,6 +7,7 @@ import Navbar from './layouts/Navbar'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
+import Youtube from './pages/Youtube'
 
 // ---------------------------------------------------------------------
 
@@ -27,6 +28,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="/youtube" element={<Youtube />} />
           </Routes>
         </div>
       </Router>
